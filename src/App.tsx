490b19909884978408c10d47a96807613415b161
KirillDanProg <kirill2898v@gmail.com>
2022-08-05 12:07:28 +0300

    state added

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,22 @@ import {Sidebar} from "./components/Sidebar/Sidebar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {Users} from "./components/Users/Users";
+import {Friends} from "./components/Friends/Friends";
+import {AppPropsType} from "./types /types";
 
 
-function App() {
+function App({state}: AppPropsType) {
     return (
         <BrowserRouter>
             <div className="App">
                 <Header/>
-                <Sidebar/>
+                <Sidebar sidebar={state.sidebar}/>
                 <div className="AppContent">
                     <Routes>
-                        <Route path="/profile" element={<Profile/>}></Route>
-                        <Route path="/dialogs" element={<Dialogs/>}></Route>
+                        <Route path="/profile" element={<Profile profile={state.profile}/>}></Route>
+                        <Route path="/dialogs" element={<Dialogs dialogs={state.dialogs}/>}></Route>
                         <Route path="/users" element={<Users/>}></Route>
+                        <Route path="/friends" element={<Friends/>}></Route>
                     </Routes>
                 </div>
             </div>
