1a1609c3ef4e50db8ef980389006e20eb59b3822
KirillDanProg <kirill2898v@gmail.com>
2022-08-08 21:49:35 +0300

    update: addMessage updateMessage handlers added

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,17 +2,11 @@ import React, {FC} from "react";
 import styles from "./Dialogs.module.css"
 import { DialogItem } from "./DialogItem/DialogItem";
 import {DialogMessage} from "./Message/DialogMessage";
-import {DialogType, MessageDataType} from "../../types /types";
+import {DialogsPropsType} from "../../types /types";
 
 
-type DialogsPropsType = {
-   dialogs: {
-       dialogsData: Array<DialogType>
-       messagesData: Array<MessageDataType>
-   }
-}
 
-export const Dialogs: FC<DialogsPropsType> = ({dialogs}) => {
+export const Dialogs: FC<DialogsPropsType> = ({dialogs, ...props}) => {
 
     const dialogElements = dialogs.dialogsData.map((dialog, i) => (
         <DialogItem key={i} name={dialog.name} img={dialog.img} id={dialog.id}/>
@@ -20,7 +14,13 @@ export const Dialogs: FC<DialogsPropsType> = ({dialogs}) => {
     const messageElements = dialogs.messagesData.map((message, i) =>  (
         <DialogMessage key={i} message={message.message} id={message.id} img={message.img}/>
     ))
+    const updateMessageText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.updateMessageText(e.currentTarget.value)
+    }
 
+    const addMessageHandler = () => {
+        props.addMessage()
+    }
     return (
         <div className={styles.dialogsContainer}>
             <div className={styles.colDialogs}>
@@ -28,6 +28,8 @@ export const Dialogs: FC<DialogsPropsType> = ({dialogs}) => {
             </div>
             <div className={styles.colMessages}>
                 {messageElements}
+                <input onChange={updateMessageText} value={dialogs.messageText} />
+                <button onClick={addMessageHandler}>send</button>
             </div>
         </div>
     )
