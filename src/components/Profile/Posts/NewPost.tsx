c770a6f0ad31739e0f81890a9a7d9c1bddf1e0cf
KirillDanProg <kirill2898v@gmail.com>
2022-08-08 21:52:42 +0300

    update: addPost updatePostText added

--- src/components/Profile/Posts/NewPost.tsx
+++ src/components/Profile/Posts/NewPost.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {FC} from "react";
 import styles from "./Posts.module.css"
+import {NewPostType} from "../../../types /types";
 
-export const NewPost = () => {
+
+export const NewPost: FC<NewPostType> = (props) => {
+    const addPost = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        props.addPost()
+    }
+
+    const updatePostTextHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.updatePostText(e.currentTarget.value)
+    }
+
     return (
         <form className={styles.form}>
-           <input className={styles.input}/>
-            <button>Add Post</button>
+           <input value={props.postText} onChange={updatePostTextHandler} className={styles.input} />
+            <button onClick={addPost}>Add Post</button>
         </form>
     )
 }
