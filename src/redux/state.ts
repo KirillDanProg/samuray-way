c13524e4937dcd8d4af26e8a2bb1c4634cb1bfd5
KirillDanProg <kirill2898v@gmail.com>
2022-08-17 23:25:02 +0300

    store created

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -4,93 +4,102 @@ import img2 from "../assets/rick.jpeg";
 import img3 from "../assets/monkey.jpeg";
 import {v1} from "uuid";
 
-let rerenderEntireTree = (x: StateType) => {
-    console.log(x)
-}
-
-export const state: StateType = {
-    dialogs: {
-        dialogsData: [
-            {id: v1(), name: "Kirill", img: img},
-            {id: v1(), name: "Alex", img: img2},
-            {id: v1(), name: "Chris", img: img3}
-        ],
-        messagesData: [
-            {id: v1(), message: "Some message text", img: img3},
-            {id: v1(), message: "Hello World", img: img2},
-            {id: v1(), message: "Lorem ipsum", img: img},
-            {id: v1(), message: "bla bla bla bla bla bla", img: img3},
-            {id: v1(), message: "Some message text", img: img2},
-        ],
-        messageText: "",
-    },
-    profile: {
-        postsData: [
-            {id: v1(), postText: "Hello world!", likes: 237, img: img},
-            {id: v1(), postText: "Bla Bla Bla", likes: 158, img: img2},
-            {id: v1(), postText: "Looking for a job", likes: 496, img: img3},
-        ],
-        profileData: {
-            name: "Kirill",
-            country: "Russia",
-            dateOfBirth: "28.09.1998"
+export const store = {
+    _state: {
+        dialogs: {
+            dialogsData: [
+                {id: v1(), name: "Kirill", img: img},
+                {id: v1(), name: "Alex", img: img2},
+                {id: v1(), name: "Chris", img: img3}
+            ],
+            messagesData: [
+                {id: v1(), message: "Some message text", img: img3},
+                {id: v1(), message: "Hello World", img: img2},
+                {id: v1(), message: "Lorem ipsum", img: img},
+                {id: v1(), message: "bla bla bla bla bla bla", img: img3},
+                {id: v1(), message: "Some message text", img: img2},
+            ],
+            messageText: "",
         },
-        postText: "",
-        error: false,
-        errorMessage: "",
-    },
-    sidebar: {
-        friends: [
-            {id: v1(), name: "Alex", img: img},
-            {id: v1(), name: "John", img: img2},
-            {id: v1(), name: "Chris", img: img3},
-        ]
-    }
-}
-
-export const addPost = () => {
-    if (state.profile.postText.trim()) {
-        const newPost = {
-            id: v1(),
-            postText: state.profile.postText,
-            likes: 0,
-            img: img
+        profile: {
+            postsData: [
+                {id: v1(), postText: "Hello world!", likes: 237, img: img},
+                {id: v1(), postText: "Bla Bla Bla", likes: 158, img: img2},
+                {id: v1(), postText: "Looking for a job", likes: 496, img: img3},
+            ],
+            profileData: {
+                name: "Kirill",
+                country: "Russia",
+                dateOfBirth: "28.09.1998"
+            },
+            postText: "",
+            error: false,
+            errorMessage: "",
+        },
+        sidebar: {
+            friends: [
+                {id: v1(), name: "Alex", img: img},
+                {id: v1(), name: "John", img: img2},
+                {id: v1(), name: "Chris", img: img3},
+            ]
         }
-        state.profile.postsData = [...state.profile.postsData, newPost]
-        state.profile.postText = ""
-        rerenderEntireTree(state)
-    } else {
-        state.profile.error = true
-        state.profile.errorMessage = "field is required"
-        rerenderEntireTree(state)
+    },
+
+    getState() {
+        return this._state
+    },
+
+    rerenderEntireTree(x: StateType) {
+        console.log(x)
+    },
+
+    subscriber(observer: (state: StateType) => void) {
+        this.rerenderEntireTree = observer
+    },
+
+    addPost() {
+        if (this._state.profile.postText.trim()) {
+            const newPost = {
+                id: v1(),
+                postText: this._state.profile.postText,
+                likes: 0,
+                img: img
+            }
+            this._state.profile.postsData = [...this._state.profile.postsData, newPost]
+            this._state.profile.postText = ""
+            this.rerenderEntireTree(this._state)
+        } else {
+            this._state.profile.error = true
+            this._state.profile.errorMessage = "field is required"
+            this.rerenderEntireTree(this._state)
+        }
+    },
+    updatePostText(newPostText: string) {
+        debugger
+        // state.profile.error = false
+        // state.profile.errorMessage = ""
+        this._state.profile.postText = newPostText
+        this.rerenderEntireTree(this._state)
+    },
+    deletePost(id: string) {
+        this._state.profile.postsData = [...this._state.profile.postsData].filter(post => post.id !== id)
+        this.rerenderEntireTree(this._state)
+    },
+    updateMessageText(newMessageText: string) {
+        this._state.dialogs.messageText = newMessageText
+        this.rerenderEntireTree(this._state)
+    },
+    addMessage() {
+        const newMessage = {
+            id: v1(),
+            message: this._state.dialogs.messageText,
+            img: img3
+        }
+        this._state.dialogs.messagesData = [...this._state.dialogs.messagesData, newMessage]
+        this._state.dialogs.messageText = ""
+        this.rerenderEntireTree(this._state)
     }
 }
-export const updatePostText = (newPostText: string) => {
-    // state.profile.error = false
-    // state.profile.errorMessage = ""
-    state.profile.postText = newPostText
-    rerenderEntireTree(state)
-}
 
-export const deletePost = (id: string) => {
-    state.profile.postsData = [...state.profile.postsData].filter(post => post.id !== id)
-    rerenderEntireTree(state)
-}
-export const updateMessageText = (newMessageText: string) => {
-    state.dialogs.messageText = newMessageText
-    rerenderEntireTree(state)
-}
-export const addMessage = () => {
-    const newMessage = {
-        id: v1(),
-        message: state.dialogs.messageText,
-        img: img3
-    }
-    state.dialogs.messagesData = [...state.dialogs.messagesData, newMessage]
-    state.dialogs.messageText = ""
-    rerenderEntireTree(state)
-}
 
-export const subscriber = (observer: (state: StateType) => void) => {
-    rerenderEntireTree = observer
-}
+
