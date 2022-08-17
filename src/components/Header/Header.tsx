67fa8bec8c8b1ece9461179f83156625addf101f
KirillDanProg <kirill2898v@gmail.com>
2022-08-17 08:39:15 +0300

    update: darkTheme toggle added step1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css"
+import ThemeToggle from "./ThemeToggle";
 
 export const Header = () => {
+    const [darkMode, setDarkMode] = React.useState(false)
+
+    const darkModeHandler = (darkMode: boolean) => {
+        setDarkMode(darkMode)
+    }
     return (
         <div className={styles.header}>
-            <Logo />
+            <Logo/>
+            <ThemeToggle darkMode={darkMode} setDarkMode={darkModeHandler}/>
         </div>
     )
 }
