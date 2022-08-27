5c95fc4d71af497935f4f76e0354e36ef016fc3d
KirillDanProg <kirill2898v@gmail.com>
2022-08-27 12:56:51 +0300

    updt: props destructuring assignment refactored

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -8,14 +8,13 @@ import {ProfilePropsType} from "../../types /ProfileType/ProfileTypes";
 
 
 
-export const Profile: FC<ProfilePropsType> = ({profile, addPost, updatePostText, deletePost}) => {
+export const Profile: FC<ProfilePropsType> = (props) => {
+    const {profile, dispatch} = props
     return (
         <div className={styles.profile}>
             <ProfileInfo profileData={profile.profileData}/>
             <Posts profile={profile}
-                   updatePostText={updatePostText}
-                   addPost={addPost}
-                   deletePost={deletePost}
+                   dispatch={dispatch}
             />
         </div>
     )
