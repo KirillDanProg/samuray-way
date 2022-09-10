ef35bd081bc22842553e97ca91bcec1994317b88
KirillDanProg <kirill2898v@gmail.com>
2022-09-10 16:00:56 +0300

    type for reducer added

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -3,6 +3,7 @@ import img3 from "../assets/monkey.jpeg";
 import {v1} from "uuid";
 import img from "../assets/images.jpeg";
 import img2 from "../assets/rick.jpeg";
+import {DialogType, MessageDataType} from "../types /DialogsType/DialogsTypes";
 
 const UPDATE_MESSAGE_TEXT = "UPDATE-MESSAGE-TEXT"
 const ADD_MESSAGE = "ADD-MESSAGE"
@@ -12,31 +13,30 @@ const initialState = {
         {id: v1(), name: "Kirill", img: img},
         {id: v1(), name: "Alex", img: img2},
         {id: v1(), name: "Chris", img: img3}
-    ],
+    ] as DialogType[],
     messagesData: [
         {id: v1(), message: "Some message text", img: img3},
         {id: v1(), message: "Hello World", img: img2},
         {id: v1(), message: "Lorem ipsum", img: img},
         {id: v1(), message: "bla bla bla bla bla bla", img: img3},
         {id: v1(), message: "Some message text", img: img2},
-    ],
-    messageText: "",
+    ] as MessageDataType[],
+    messageText: "" as string,
 }
 
-const dialogsReducer = (state: any = initialState, action: ActionsType): any => {
+type InitialStateType = typeof initialState
+
+const dialogsReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
         case UPDATE_MESSAGE_TEXT:
-            state.messageText = action.payload
-            break;
+           return {...state, messageText : action.payload.newMessageText}
         case ADD_MESSAGE:
             const newMessage = {
                 id: v1(),
                 message: state.messageText,
                 img: img3
             }
-            state.messagesData = [...state.messagesData, newMessage]
-            state.messageText = ""
-            break;
+           return {...state, messagesData: [...state.messagesData, newMessage], messageText: ""}
         default:
             return state
     }
@@ -45,7 +45,9 @@ const dialogsReducer = (state: any = initialState, action: ActionsType): any => 
 export const updateMessageTextAC = (newMessageText: string) => {
     return {
         type: UPDATE_MESSAGE_TEXT,
-        payload: newMessageText
+        payload: {
+            newMessageText,
+        }
     } as const
 }
 
