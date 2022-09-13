43ef7c9494d3fa4f5a013f3cd9be49498936198c
KirillDanProg <kirill2898v@gmail.com>
2022-09-13 16:11:31 +0300

    users-reducer added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,20 @@
 import {combineReducers, createStore} from "redux";
-import dialogsReducer from "./dialogs-reducer";
-import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogsReducer/dialogs-reducer";
+import profileReducer from "./profileReducer/profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
+import {usersReducer} from "./usersReducer/users-reducer";
 
 const rootReducer: AppType = combineReducers({
     dialogs: dialogsReducer,
     profile: profileReducer,
     sidebar: sidebarReducer,
+    users: usersReducer
 })
 
-export type AppType = DialogsReducerType & ProfileReducerType & SidebarReducerType
+export type AppType = DialogsReducerType & ProfileReducerType & SidebarReducerType & UsersReducerType
 type DialogsReducerType = ReturnType<typeof dialogsReducer>
 type ProfileReducerType = ReturnType<typeof profileReducer>
 type SidebarReducerType = ReturnType<typeof sidebarReducer>
+type UsersReducerType = ReturnType<typeof usersReducer>
 
 export const store = createStore(rootReducer)
