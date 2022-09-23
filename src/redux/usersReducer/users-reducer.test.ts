c9bcc2cc12c8983540aea0258c910ac5cff4d2a5
KirillDanProg <kirill2898v@gmail.com>
2022-09-23 09:48:09 +0300

    test state moved to top

--- src/redux/usersReducer/users-reducer.test.ts
+++ src/redux/usersReducer/users-reducer.test.ts
@@ -1,21 +1,27 @@
 import {v1} from "uuid";
-import {followACType, setUserACType, unfollowACType, User, usersReducer} from "./users-reducer";
+import {followACType, InitialUsersStateType, setUserACType, unfollowACType, usersReducer} from "./users-reducer";
 
+let initialState: InitialUsersStateType
+
+beforeEach(() => {
+    initialState = {
+        users: [
+            {id: v1(), name: "Alex", followed: false},
+            {id: v1(), name: "Kirill", followed: true}]
+        ,
+        error: "",
+        total: 100,
+        count: 10,
+        page: 1
+    }
+})
 
 test("users should be uploaded", () => {
-    const initialState = {
-        users: [] as User[],
-        error: "" as string,
-        totalCount: 5 as number
-    }
-    const users = [
-        {id: v1(), name: "Alex", followed: false},
-        {id: v1(), name: "Kirill", followed: false}
-    ]
+
     const action: setUserACType = {
         type: "SET-USERS",
         payload: {
-            users: users
+            users: initialState.users
         }
     }
 
@@ -28,16 +34,7 @@ test("users should be uploaded", () => {
 })
 
 test("user followed should be true", () => {
-    const initialState = {
-        users: [
-            {id: v1(), name: "Alex", followed: false},
-            {id: v1(), name: "Kirill", followed: false}
-        ] as User[],
-        error: "" as string,
-        totalCount: 5 as number
-    }
-
-    const action: followACType= {
+    const action: followACType = {
         type: "FOLLOW",
         payload: {
             id: initialState.users[0].id
@@ -50,21 +47,13 @@ test("user followed should be true", () => {
 })
 
 test("user followed should be false", () => {
-    const initialState = {
-        users: [
-            {id: v1(), name: "Alex", followed: false},
-            {id: v1(), name: "Kirill", followed: true}
-        ] as User[],
-        error: "" as string,
-        totalCount: 5 as number
-    }
+
     const action: unfollowACType = {
         type: "UNFOLLOW",
         payload: {
             id: initialState.users[1].id
         }
     }
-
     const newState = usersReducer(initialState, action)
 
     expect(newState.users[1].followed).toBe(false)
