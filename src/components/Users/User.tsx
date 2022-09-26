b4b5a065a1bc0fe1431b433bd19a1e92cd3596a4
KirillDanProg <kirill2898v@gmail.com>
2022-09-26 22:21:59 +0300

    link to profile on user click img added

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -1,10 +1,11 @@
 import React, {FC} from "react";
 import styles from "./Users.module.css";
-import userPh from "../../assets/userPh.jpeg";
+import {NavLink} from "react-router-dom";
+
 type UserPropsType = {
     id: string
     name: string
-    photo?: string
+    photos:  string | null
     followed: boolean
     status?: string
     follow: (id: string) => void
@@ -14,7 +15,9 @@ type UserPropsType = {
 export const User: FC<UserPropsType> = (props) => {
     return (
         <div className={styles.userBox}>
-            <img style={{width: "80px", borderRadius: "50%"}} src={props.photo ? props.photo : userPh}/>
+            <NavLink to={`/profile/${props.id}`}>
+                <img style={{width: "80px", borderRadius: "50%"}} src={props.photos ? props.photos : "https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png"}/>
+            </NavLink>
             <div>{props.name}</div>
             <div>{props.status}</div>
 
