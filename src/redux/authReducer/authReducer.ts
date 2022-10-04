0e51a75e808f21d0ce9012601a5812ac747bc5dc
KirillDanProg <kirill2898v@gmail.com>
2022-10-04 10:08:52 +0300

    authReducer added with test

---
+++ src/redux/authReducer/authReducer.ts
@@ -0,0 +1,41 @@
+
+export type initialAuthStateType = {
+    id: number | null
+    email: string | null
+    login: string | null
+}
+const initialState = {
+    id: null,
+    email: null,
+    login: null
+}
+type ActionsType = ReturnType<typeof authMe>
+
+export const USER_AUTH = "USER-AUTH"
+
+
+export const authReducer = (state: initialAuthStateType = initialState, action: ActionsType): initialAuthStateType => {
+    switch(action.type) {
+        case USER_AUTH:
+            return {
+                ...state,
+                id: action.payload.id,
+                login: action.payload.login,
+                email: action.payload.email,
+            }
+        default:
+            return state
+    }
+}
+
+
+export const authMe = (id: number, login: string, email: string) => {
+    return {
+        type: USER_AUTH,
+        payload: {
+            id,
+            email,
+            login,
+        }
+    } as const
+}
