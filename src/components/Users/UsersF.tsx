b8273122b3997c1ea16a0de2bb8778a842d40304
KirillDanProg <kirill2898v@gmail.com>
2022-10-04 12:57:08 +0300

    follow/unfollow request added

--- src/components/Users/UsersF.tsx
+++ src/components/Users/UsersF.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
 import {UsersPropsType} from "./UsersContainer";
 import styles from './Users.module.css'
@@ -12,17 +12,41 @@ type UsersPropsTypeTest = {
 export const Users = (props: UsersPropsTypeTest) => {
     const {setUsers, users, changePage, follow, unfollow} = props.props
     useEffect(() => {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${users.page}&count=${users.count}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${users.page}&count=${users.count}`, {
+            withCredentials: true
+        })
             .then(response => {
                 setUsers(response.data.items)
             })
     }, [users.page, setUsers, users.count])
 
     const followHandler = (id: string) => {
-        follow(id)
+        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': "98c53cec-c5b7-4a19-9abc-502320b1a878"
+            }
+        })
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    follow(id)
+                }
+            })
+
     }
     const unfollowHandler = (id: string) => {
-        unfollow(id)
+        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': "98c53cec-c5b7-4a19-9abc-502320b1a878"
+            }
+        })
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    unfollow(id)
+                }
+            })
+
     }
 
 
