4a643e85a37265ffbec473daf6dba1f99e8abfba
KirillDanProg <kirill2898v@gmail.com>
2022-10-09 23:33:48 +0300

    profile types updated

--- src/types /ProfileType/ProfileTypes.ts
+++ src/types /ProfileType/ProfileTypes.ts
@@ -7,15 +7,13 @@ export type ProfileType = {
 }
 
 export type ProfileDataType = {
-    data: {
-        id: string
+    userId: string
         fullName: string
         photos: {small?: string | null, large?: string | null}
         followed: boolean
         status?: string
         follow?: (id: string) => void
         unfollow?: (id: string) => void
-    }
 }
 export type PostDataType = {
     id: string
