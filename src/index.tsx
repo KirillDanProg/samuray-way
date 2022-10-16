d8b25b73fad4ce1486b2a44ace76a251156751c6
KirillDanProg <kirill2898v@gmail.com>
2022-10-16 22:56:58 +0300

    react-scripts and react updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import App from "./App";
-import ReactDOM from "react-dom";
 import {store} from "./redux/store";
 import {Provider} from "react-redux";
+import { createRoot } from 'react-dom/client';
 
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
     <Provider store={store}>
         <App/>
     </Provider>
-    , document.getElementById('root')
 );
-
-
