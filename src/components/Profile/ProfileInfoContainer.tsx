cca284899e369fb6de09a525efd0a6525044bd4b
KirillDanProg <kirill2898v@gmail.com>
2022-10-16 22:59:57 +0300

    callbacks added to MDTP

--- src/components/Profile/ProfileInfoContainer.tsx
+++ src/components/Profile/ProfileInfoContainer.tsx
@@ -3,7 +3,7 @@ import {connect} from "react-redux";
 import {ProfileDataType} from "../../types /ProfileType/ProfileTypes";
 import {AppType} from "../../redux/store";
 import {ProfileInfo} from "./ProfileInfo";
-import {getProfileDataTC} from "../../redux/profileReducer/profile-reducer";
+import {changeUserStatusTC, getProfileDataTC, getUserStatusTC} from "../../redux/profileReducer/profile-reducer";
 import {AnyAction} from "redux";
 import {ThunkDispatch} from "redux-thunk";
 import {withRouter} from "../../hoc/withRouter";
@@ -19,22 +19,33 @@ export type ProfileInfoContainerPropsType = MapStatePropsType & MapDispatchType 
 }
 
 class ProfileInfoContainer extends React.Component<ProfileInfoContainerPropsType> {
-    componentDidMount() {
-        const id = Number(this.props.router.params.userId)
+    initData = () => {
         const defaultID = this.props.authID
+        const id = Number(this.props.router.params.userId)
         this.props.getProfileData(id ? id : defaultID)
+        this.props.getUserStatus(id ? id : defaultID)
+    }
+
+    componentDidMount() {
+        this.initData()
     }
 
     componentDidUpdate(prevProps: Readonly<ProfileInfoContainerPropsType>) {
-        if(prevProps.authID !== this.props.authID) {
-            const defaultID = this.props.authID
-            this.props.getProfileData(defaultID)
+        if (prevProps.authID !== this.props.authID) {
+            this.initData()
         }
+        if (prevProps.profileData.status !== this.props.profileData.status) {
+            this.changeUserStatus(this.props.profileData.status)
+        }
+    }
+
+    changeUserStatus = (status: string | undefined) => {
+        this.props.updateUserStatus(status)
     }
 
     render() {
         return (
-            <ProfileInfo profileData={this.props.profileData}/>
+            <ProfileInfo profileData={this.props.profileData} changeStatus={this.changeUserStatus}/>
         )
     }
 }
@@ -46,19 +57,29 @@ type MapStatePropsType = {
 }
 type MapDispatchType = {
     getProfileData: (userId: number) => void
+    updateUserStatus: (status: string | undefined) => void
+    getUserStatus: (id: number) => void
 }
+
 
 const mapStateToProps = (state: AppType): MapStatePropsType => {
     return {
         profileData: state.profile.profileData,
-        authID: state.auth.id
+        authID: state.auth.id,
     }
 }
 
-const mapDispatchToProps = (dispatch: ThunkDispatch<AppType, void, AnyAction>) => {
+const mapDispatchToProps = (dispatch: ThunkDispatch<AppType, void, AnyAction>): MapDispatchType => {
     return {
         getProfileData: (userId: number) => {
             dispatch(getProfileDataTC(userId))
+        },
+        updateUserStatus: (status: string | undefined) => {
+            dispatch(changeUserStatusTC(status))
+        },
+        getUserStatus: (id: number) => {
+
+            dispatch(getUserStatusTC(id))
         }
     }
 }
