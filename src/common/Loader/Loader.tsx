413a8cb4bff94a6357f02c8912c9c6a44a1aa41e
KirillDanProg <kirill2898v@gmail.com>
2022-10-29 23:58:20 +0300

    update: PreLoader

--- src/common/Loader/Loader.tsx
+++ src/common/Loader/Loader.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from "./Loader.module.css"
 
 const Loader = () => {
     return (
-        <div>
-            ...LOADING
+        <div className={`${styles.loading} ${styles.loading01}`}>
+            <span>L</span>
+            <span>O</span>
+            <span>A</span>
+            <span>D</span>
+            <span>I</span>
+            <span>N</span>
+            <span>G</span>
         </div>
     );
 };
